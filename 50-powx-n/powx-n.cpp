3b27a5c7daf0c45596efb36f4a2ef492b18cf024
Jahnvi Vanzara <140196434+Jahnvi1905@users.noreply.github.com>
2026-05-20 14:55:05 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (92.43%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -1,3 +1,23 @@
+// class Solution {
+// public:
+//     double myPow(double x, int n) {
+
+//         long long N = n;
+
+//         if(N < 0) {
+//             x = 1 / x;
+//             N = -N;
+//         }
+//         double ans=1;
+//         while(N>0){
+//             ans=ans*x;
+//             N--;
+//         }    
+    
+//         return ans;
+//     }
+// };
+
 /*
 Time Complexity: O(log n)
 Space Complexity: O(1)
