9aaded1a87e1fa577b25bfd9c9b64e22c859d1d7
Jahnvi Vanzara <140196434+Jahnvi1905@users.noreply.github.com>
2026-07-07 10:27:31 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (10.61%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,12 +1,18 @@
 class Solution {
 public:
-int recursion(int n ){
+int recursion(int n,vector<int>&dp ){
     if(n==0) return 0;
     if(n==1) return 1;
 
-    return recursion(n-1) + recursion(n-2);
+    if(dp[n] !=-1){
+        return dp[n];
+    }
+
+    dp[n] = recursion(n-1,dp) + recursion(n-2,dp);
+    return dp[n];
 }
     int fib(int n) {
-        return recursion(n);
+        vector<int>dp(n+1,-1);
+        return recursion(n,dp);
     }
 };
