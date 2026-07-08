55b5ace6f10337b19d8f124dc33faa5cf66c0f29
Jahnvi Vanzara <140196434+Jahnvi1905@users.noreply.github.com>
2026-07-08 14:49:47 +0530

    Time: 29 ms (57.96%) | Memory: 60.4 MB (73.08%) - LeetSync

---
+++ 630-course-schedule-iii/course-schedule-iii.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int scheduleCourse(vector<vector<int>>& courses) {
+
+        sort(courses.begin(), courses.end(),
+             [](auto &a, auto &b){
+                 return a[1] < b[1];
+             });
+
+        priority_queue<int> pq;
+
+        int time = 0;
+
+        for(auto &course : courses){
+
+            time += course[0];
+            pq.push(course[0]);
+
+            if(time > course[1]){
+                time -= pq.top();
+                pq.pop();
+            }
+        }
+
+        return pq.size();
+    }
+};
