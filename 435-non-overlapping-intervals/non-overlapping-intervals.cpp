3503f464e8444f741c2144a6617b7646e9193041
Jahnvi Vanzara <140196434+Jahnvi1905@users.noreply.github.com>
2026-07-08 14:10:56 +0530

    Time: 50 ms (48.51%) | Memory: 94 MB (68.95%) - LeetSync

---
+++ 435-non-overlapping-intervals/non-overlapping-intervals.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+
+    static bool cmp(vector<int>& a, vector<int>& b) {
+        return a[1] < b[1];
+    }
+
+    int eraseOverlapIntervals(vector<vector<int>>& intervals) {
+
+        sort(intervals.begin(), intervals.end(), cmp);
+
+        int remove = 0;
+        int prevEnd = intervals[0][1];
+
+        for (int i = 1; i < intervals.size(); i++) {
+
+            if (intervals[i][0] >= prevEnd) {
+                // No overlap
+                prevEnd = intervals[i][1];
+            }
+            else {
+                // Overlap
+                remove++;
+            }
+        }
+
+        return remove;
+    }
+};
