9eb7a73372c581801d3f9a1ba488e6ba851afeba
Jahnvi Vanzara <140196434+Jahnvi1905@users.noreply.github.com>
2026-07-31 18:21:51 +0530

    Time: 48 ms (46.66%) | Memory: 45.4 MB (71.21%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -1,23 +1,48 @@
-class Solution{
-public:
-    int subarraySum(vector<int> &nums, int k){
+// class Solution{
+// public:
+//     int subarraySum(vector<int> &nums, int k){
         
-        int n = nums.size();
+//         int n = nums.size();
+//         int count = 0;
+
+//         for(int i = 0; i < n; i++)
+//         {
+//             int sum = 0;
+
+//             for(int j = i; j < n; j++)
+//             {
+//                 sum += nums[j];
+
+//                 if(sum == k)
+//                 {
+//                     count++;
+//                 }
+//             }
+//         }
+
+//         return count;
+//     }
+// };
+
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+
+        unordered_map<int,int> mp;
+
+        mp[0] = 1;
+
+        int prefixSum = 0;
         int count = 0;
 
-        for(int i = 0; i < n; i++)
-        {
-            int sum = 0;
+        for(int num : nums){
 
-            for(int j = i; j < n; j++)
-            {
-                sum += nums[j];
+            prefixSum += num;
 
-                if(sum == k)
-                {
-                    count++;
-                }
-            }
+            if(mp.find(prefixSum - k) != mp.end())
+                count += mp[prefixSum - k];
+
+            mp[prefixSum]++;
         }
 
         return count;
