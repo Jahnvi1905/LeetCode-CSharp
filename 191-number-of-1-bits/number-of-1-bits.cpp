e0e6c29f33851e9a4b747e2fd17f898f02136902
Jahnvi Vanzara <140196434+Jahnvi1905@users.noreply.github.com>
2026-08-01 14:52:30 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (49.29%) - LeetSync

---
+++ 191-number-of-1-bits/number-of-1-bits.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int hammingWeight(int n) {
+       int cnt=0;
+
+        while(n){
+
+            n &= (n-1);
+
+            cnt++;
+        }
+
+        return cnt; 
+    }
+};
