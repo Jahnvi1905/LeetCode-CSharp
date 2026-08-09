6d082fc7df88b6b5751b843f054289f896e1e47d
Jahnvi Vanzara <140196434+Jahnvi1905@users.noreply.github.com>
2026-08-09 16:00:43 +0530

    Time: 9 ms (61.36%) | Memory: 13.8 MB (43.80%) - LeetSync

---
+++ 1240-stone-game-ii/stone-game-ii.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    int stoneGameII(vector<int>& piles) {
+        int n = piles.size();
+
+        // suffix[i] = sum of piles from i to n-1
+        vector<int> suffix(n + 1, 0);
+
+        for (int i = n - 1; i >= 0; i--) {
+            suffix[i] = suffix[i + 1] + piles[i];
+        }
+
+        // dp[i][M] = maximum stones current player can get
+        // starting from i with M
+        vector<vector<int>> dp(n + 1, vector<int>(n + 1, 0));
+
+        for (int i = n - 1; i >= 0; i--) {
+
+            for (int M = 1; M <= n; M++) {
+
+                // Can take all remaining piles
+                if (2 * M >= n - i) {
+                    dp[i][M] = suffix[i];
+                    continue;
+                }
+
+                int best = 0;
+
+                // Try taking X piles
+                for (int X = 1; X <= 2 * M && i + X <= n; X++) {
+
+                    int nextM = max(M, X);
+
+                    int opponent = dp[i + X][nextM];
+
+                    int current = suffix[i] - opponent;
+
+                    best = max(best, current);
+                }
+
+                dp[i][M] = best;
+            }
+        }
+
+        return dp[0][1];
+    }
+};
