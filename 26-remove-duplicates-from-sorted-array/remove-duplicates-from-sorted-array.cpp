98abd30095b1258db2dbd00d98e802c9b08c8c4d
Jahnvi Vanzara <140196434+Jahnvi1905@users.noreply.github.com>
2026-08-22 13:00:08 +0530

    Time: 51 ms (5.34%) | Memory: 22.7 MB (53.10%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
@@ -1,21 +1,36 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        if (nums.empty()) return 0;
+        // if (nums.empty()) return 0;
         
-        int start = 0; // Pointer 1: Unique elements ko track karega
-        int i = 1;     // Pointer 2: Pure array ko scan karega
+        // int start = 0; // Pointer 1: Unique elements ko track karega
+        // int i = 1;     // Pointer 2: Pure array ko scan karega
         
-        while (i < nums.size()) {
-            // Agar naya unique element milta hai
-            if (nums[i] != nums[start]) {
-                start++;                 // Unique boundary ko badhao
-                nums[start] = nums[i];   // Naye element ko insert karo
+        // while (i < nums.size()) {
+        //     // Agar naya unique element milta hai
+        //     if (nums[i] != nums[start]) {
+        //         start++;                 // Unique boundary ko badhao
+        //         nums[start] = nums[i];   // Naye element ko insert karo
+        //     }
+        //     i++; // Scanning pointer hamesha aage badhega
+        // }
+        
+        // // Total unique elements 'start + 1' honge
+        // return start + 1;
+
+        if(nums.empty()) return 0;
+
+        int i=0;
+        for (i=0 ; i<nums.size();i++){
+            for(int j=i+1 ; j<nums.size() ;){
+                if(nums[i]== nums[j]){
+                    nums.erase(nums.begin()+j);
+                }
+                else{
+                    j++;
+                }
             }
-            i++; // Scanning pointer hamesha aage badhega
         }
-        
-        // Total unique elements 'start + 1' honge
-        return start + 1;
+        return nums.size();
     }
 };
