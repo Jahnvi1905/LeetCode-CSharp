c8e21cac0812dd09deb0490c5751dce3b0140917
Jahnvi Vanzara <140196434+Jahnvi1905@users.noreply.github.com>
2026-09-08 19:35:20 +0530

    Time: 3 ms (67.26%) | Memory: 15 MB (28.26%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,19 +1,47 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-      int n = nums.size();
+    //   int n = nums.size();
 
-        for(int i = 0; i < n; i++)
-        {
-            for(int j = i + 1; j < n; j++)
-            {
-                if(nums[i] + nums[j] == target)
-                {
-                    return {i, j};
-                }
-            }
+    //     for(int i = 0; i < n; i++)
+    //     {
+    //         for(int j = i + 1; j < n; j++)
+    //         {
+    //             if(nums[i] + nums[j] == target)
+    //             {
+    //                 return {i, j};
+    //             }
+    //         }
+    //     }
+
+    //     return {};  
+
+    // unordered_map<int,int> mp;
+
+    //     for(int i=0;i<nums.size();i++){
+
+    //         int need = target - nums[i];
+
+    //         if(mp.count(need))
+    //             return {mp[need], i};
+
+    //         mp[nums[i]] = i;
+    //     }
+
+    //     return {};
+
+    unordered_map<int,int> mp ;
+
+    for(int i=0;i<=nums.size();i++){
+
+        int need = target - nums[i];
+
+        if(mp.find(need) != mp.end()){
+            return {mp[need],i};
         }
 
-        return {};  
+        mp[nums[i]] =i;
+    }
+return {};
     }
 };
